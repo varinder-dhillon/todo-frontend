3bcc0846bcf0dbce9e5441d49077eb4ded0f48af
Varinder Dhillon <varinderdhilloninfo@gmail.com>
2025-05-17 15:35:37 +0530

    Setup (App): Added the assets

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import store from './store/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    |<Provider store={store}>
+    <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
